b87aac663d96544cd45f9c563ec731e7e7708961
Ishan Dev <braincodeishan@gmail.com>
2023-07-13 02:00:53 +0530

    new changes by ishan product page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,9 +1,26 @@
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Container, Grid, Title, Image, Text } from "@mantine/core";
+import { useParams } from "react-router"
+import { useGetProductQuery } from '../store/products/productsSlice';
 const ProductDetail = () => {
+  const {id}=useParams()
+  console.log(id);
+  const { data: productData, error, isLoading } = useGetProductQuery(1);
+  console.log(productData);
   return (
-    <div>
-      <Link to="/">Go to home page</Link>ProductDetail
-    </div>
+    <Container fluid>
+      <Grid>
+        <Grid.Col span={6}>
+          <Image src={productData.image} height={'auto'} width={'80%'} alt="ProductImage" />
+        </Grid.Col>
+        <Grid.Col span={6}>
+            <Title>{productData.title}</Title>
+            
+            <Text fz="xl">$ {productData.price}</Text>
+            <Text fz='md'>{productData.description}</Text>
+        </Grid.Col>
+      </Grid>
+    </Container>
   );
 };
 
--- src/store/products/productsSlice.js
+++ src/store/products/productsSlice.js
@@ -10,7 +10,13 @@ export const productsApi = createApi({
     getProducts: builder.query({
       query: () => `products`,
     }),
+    getProduct: builder.query({
+      query: (id) => `products/${id}`,
+    }),
   }),
 });
 
-export const { useGetProductsQuery } = productsApi;
+
+
+export const { useGetProductsQuery, useGetProductQuery } = productsApi;
+
