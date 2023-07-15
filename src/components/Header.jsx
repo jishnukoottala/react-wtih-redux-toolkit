e0793d3e4cd8cb1ecc50e4269eed0cb9caa89032
Jishnu Koottala <jishnu61@gmail.com>
2023-07-15 09:14:31 +0530

    updated site title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       {/* <Title order={2}>React Redux Shopping Cart</Title> */}
-      <Title order={2}>Redux toolkit demo</Title>
+      <Title order={2}>My Kart</Title>
     </div>
   );
 };
